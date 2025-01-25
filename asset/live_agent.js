0f917bca2955e4ab93567ba7626531232bfaefef
Matteo Gevi <matteogevi97@gmail.com>
2025-01-25 20:42:45 +0100

    FORMICAIO js inside asset

--- asset/live_agent.js
+++ asset/live_agent.js
@@ -362,11 +362,11 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 500);
     }
 
-    const backendURL = window.location.hostname === 'localhost' ? 'http://localhost:5031' : 'https://formicaio-99c83a293f10.herokuapp.com/';
+    const backendURL = window.location.hostname === 'localhost' ? 'http://localhost:5033' : 'https://formicaio-99c83a293f10.herokuapp.com/';
     
     async function sendMessageToBackend(message) {
         try {
-            const response = await fetch('http://localhost:5031/ask', {
+            const response = await fetch('http://localhost:5033/ask', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',  
--- code_base.py
+++ code_base.py
@@ -57,5 +57,5 @@ def ask():
         return jsonify({"response": f"Error: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5031))
+    port = int(os.getenv("PORT", 5033))
     app.run(debug=True, host="0.0.0.0", port=port)
